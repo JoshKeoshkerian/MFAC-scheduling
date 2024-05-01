e53dc2b863dbdbe457c9a33ed979a6c3f3f7f14f
JKEOSHKE3745 <JKEOSHKE3745@W11-9RM2CS3>
2024-05-01 14:10:07 -0500

    Add project files.

---
+++ MFAC scheduling/Form1.Designer.vb
@@ -0,0 +1,71 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class WorkApp
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnExtract = New System.Windows.Forms.Button()
+        Me.txtLink = New System.Windows.Forms.TextBox()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnExtract
+        '
+        Me.btnExtract.Location = New System.Drawing.Point(215, 107)
+        Me.btnExtract.Name = "btnExtract"
+        Me.btnExtract.Size = New System.Drawing.Size(75, 23)
+        Me.btnExtract.TabIndex = 0
+        Me.btnExtract.Text = "Extract"
+        Me.btnExtract.UseVisualStyleBackColor = True
+        '
+        'txtLink
+        '
+        Me.txtLink.Location = New System.Drawing.Point(205, 152)
+        Me.txtLink.Name = "txtLink"
+        Me.txtLink.Size = New System.Drawing.Size(100, 22)
+        Me.txtLink.TabIndex = 1
+        '
+        'lblOutput
+        '
+        Me.lblOutput.Location = New System.Drawing.Point(484, 139)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(250, 193)
+        Me.lblOutput.TabIndex = 2
+        '
+        'WorkApp
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.txtLink)
+        Me.Controls.Add(Me.btnExtract)
+        Me.Name = "WorkApp"
+        Me.Text = "WorkApp"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnExtract As Button
+    Friend WithEvents txtLink As TextBox
+    Friend WithEvents lblOutput As Label
+End Class
---
+++ MFAC scheduling/Form1.vb
@@ -0,0 +1,148 @@
+﻿Imports System.Threading
+Imports System.IO
+Imports System.Net
+Imports HtmlAgilityPack
+Public Class WorkApp
+
+    Public Class person
+        Public Property name As String
+        Public Property timeIn As Integer
+        Public Property timeOut As Integer
+    End Class
+    Dim listData As IList(Of String)
+    Public Sub getData()
+
+        Thread.Sleep(3000)
+        AppActivate("google chrome")
+        Thread.Sleep(300)
+        My.Computer.Keyboard.SendKeys("^u", True)
+        Thread.Sleep(300)
+        My.Computer.Keyboard.SendKeys("^a", True)
+        Thread.Sleep(300)
+        My.Computer.Keyboard.SendKeys("^c", True)
+        Thread.Sleep(300)
+        My.Computer.Keyboard.SendKeys("^w", True)
+
+        Dim data As String = My.Computer.Clipboard.GetText()
+
+
+        listData = New List(Of String)(data.Split(New Char() {vbCrLf, vbCr, vbLf}, StringSplitOptions.RemoveEmptyEntries))
+
+        AppActivate("WorkApp")
+    End Sub
+
+    Function GetTimeIn(ByVal inputString As String) As String
+        Dim firstNum As Integer
+        Dim endNum As Integer
+        Dim newString As String
+        For Each i As Char In inputString
+            If i Like "#" Then
+                firstNum = inputString.IndexOf(i)
+                newString = inputString.Substring(firstNum, ((inputString.Length - 1) - firstNum))
+                Exit For
+            End If
+        Next
+        For Each i As Char In newString
+            If i Like "[a-z]" Then
+                endNum = newString.IndexOf(i)
+                Exit For
+            End If
+        Next
+        'Debug.Print(newString.Substring(0, (endNum + 1)))
+
+        Return newString.Substring(0, (endNum + 1))
+    End Function
+    Public Function timeConverter(ByVal i As String)
+        Dim key As Integer
+        Dim newI As Integer
+        If i.Contains(":") Then
+            key = i.IndexOf(":")
+            i.Remove(key, 3)
+            newI = Val(i) + 1
+        End If
+        If i.Contains("12") Then
+            Return 12
+        End If
+        If i.Contains("a") Then
+            key = i.IndexOf("a")
+            i.Substring(0, key)
+            Debug.Print("step: " & i)
+            newI = Val(i)
+        ElseIf i.Contains("p") Then
+
+            key = i.IndexOf("p")
+            i.Substring(0, key)
+            newI = Val(i) + 12
+        End If
+
+        Return newI
+
+    End Function
+
+    Function nameConverter(ByVal i As String)
+        Dim num As Integer
+
+        For Each x As Char In i
+            If x Like "#" Then
+                num = i.IndexOf(x)
+                Exit For
+
+            End If
+        Next
+
+        Return i.Substring(1, (num - 1))
+        'Return (i.Split(" ")(0) + " " + i.Split(" ")(1)).Remove(0, 1)
+    End Function
+    Public Sub storeData(ByVal i As String)
+        Dim name As String = nameConverter(i)
+        Debug.Print(name)
+        Dim timeIn As String = GetTimeIn(i)
+        timeIn = timeConverter(timeIn)
+        Debug.Print(timeIn)
+
+
+
+    End Sub
+
+    Public Sub parceData()
+            Dim lifeguardflag As Boolean = False
+
+
+            For Each i As String In listData
+                If i.Length > 10 Then
+                    If i.Substring(0, 11) = "<script>php" Then
+                        Dim tempStr As String() = i.Split($"{Chr(34)}")
+                        If tempStr(5) = "Lifeguard-MFAC" Then
+                        lifeguardflag = True
+                        i = i.Split(",")(10)
+
+                        If i(1) Like "[A-Z]" Then
+                            storeData(i)
+                        End If
+
+                        Continue For
+                    End If
+                    End If
+
+                    If lifeguardflag = True Then
+                    If i.Substring(0, 11) = "<script>hdr" Then
+                        Dim strLifeguard As String = i.Split(",")(7)
+
+                        If strLifeguard(1) Like "[A-Z]" Then
+                            storeData(strLifeguard)
+                        End If
+                    Else
+                        lifeguardflag = False
+                    End If
+                End If
+                End If
+
+            Next
+        End Sub
+
+        Private Sub btnExtract_Click(sender As Object, e As EventArgs) Handles btnExtract.Click
+
+        getData()
+        parceData()
+        End Sub
+    End Class
